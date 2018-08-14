01a436f19ae74a4819e7e42fed276a4e55611aa2
CindyvdVries <cindyvdvries@gmail.com>
2018-08-14 11:00:41 +0200

    alignment colofon text

--- src/components/VColofon.vue
+++ src/components/VColofon.vue
@@ -8,11 +8,11 @@
     </v-toolbar>
     <v-card small>
       <v-card-title primary-title>
-        <div>
-          Voor algemene vragen over de vegetatiemonitor en de achtergronden bij deze ontwikkeling <a href="mailto:stanford.wilson@rws.nl?subject=Vegetatiemonitor">Stanford Wilson</a> <br>
-          Voor technische vragen: <a href="mailto:ellis.penning@deltares.nl?subject=Vegetatiemonitor">Ellis Penning</a> <br>
-          <a href="static/docs/TRM.pdf" download>Technische referentie handleiding</a>
-        </div>
+        <pre>
+Voor algemene vragen over de vegetatiemonitor en de achtergronden bij deze ontwikkeling: <a href="mailto:stanford.wilson@rws.nl?subject=Vegetatiemonitor">Stanford Wilson</a>
+Voor technische vragen: <a href="mailto:ellis.penning@deltares.nl?subject=Vegetatiemonitor">Ellis Penning</a>
+Download <a href="static/docs/TRM.pdf" download>hier</a> de Technische referentie handleiding.
+        </pre>
       </v-card-title>
     </v-card>
   </v-expansion-panel-content>
@@ -20,7 +20,8 @@
 </template>
 
 <style scoped>
-.v-colofon {
+pre {
+  white-space: pre-wrap;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: left;
 }
