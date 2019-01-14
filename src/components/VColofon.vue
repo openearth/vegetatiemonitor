cc0aba131d12ec752d86e3bf3cfa5f6e56907fbf
CindyvdVries <cindyvdvries@gmail.com>
2019-01-14 11:44:18 +0100

    Update VColofon.vue
    Added youtube movie to colofon

--- src/components/VColofon.vue
+++ src/components/VColofon.vue
@@ -12,6 +12,7 @@
 Voor algemene vragen over de vegetatiemonitor en de achtergronden bij deze ontwikkeling: <a href="mailto:stanford.wilson@rws.nl?subject=Vegetatiemonitor">Stanford Wilson</a>
 Voor technische vragen: <a href="mailto:ellis.penning@deltares.nl?subject=Vegetatiemonitor">Ellis Penning</a>
 Download <a href="static/docs/TRM.pdf" download>hier</a> de Technische referentie handleiding.
+Klik <a href="https://www.youtube.com/watch?v=mOjCRgauxyM&feature=youtu.be">hier</a> voor het instructiefilmpje. 
         </pre>
       </v-card-title>
     </v-card>
