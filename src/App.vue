542fae3a4bf2fedaea0f5bb9bc95dd866fda3067
CindyvdVries <cindyvdvries@gmail.com>
2019-05-10 13:41:02 +0200

    refactor toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,19 @@
 <template>
   <v-app>
-    <v-toolbar dense color="primary" fixed app>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title class="hidden-sm-and-down"
-        >Vegetatie Monitor</v-toolbar-title
-      >
-      <v-spacer />
-      <v-flex xs-6>
-        <v-tabs color="primary" fixed-tabs>
-          <v-tab v-for="mode in modes" :key="mode">
-            {{ mode }}
-          </v-tab>
-        </v-tabs>
-      </v-flex>
-      <v-spacer />
-      <v-img class="hidden-sm-and-down" src="images/deltares.svg" />
-      <v-img class="hidden-sm-and-down" src="images/Rijkswaterstaat.svg" />
-    </v-toolbar>
+    <toolbar />
   </v-app>
 </template>
 
 <script>
+import Toolbar from './components/Toolbar'
 export default {
   name: 'app',
   data: function() {
-    return {
-      modes: ['Veld', 'Verken', 'Voorspel']
-    }
+    return {}
   },
-  components: {}
+  components: {
+    Toolbar
+  }
 }
 </script>
 
