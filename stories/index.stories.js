3f542d6bf4be2a74454837471267a27adbd3374c
Fedor Baart <fedor.baart@deltares.nl>
2019-09-20 00:45:33 +0200

    refactor timeslider

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,14 +4,42 @@ import { storiesOf } from "@storybook/vue"
 
 import Vue from "vue"
 
-import Vuetify from 'vuetify'
+import vuetify from '../src/plugins/vuetify'
+import moment from 'moment'
+
 import 'vuetify/dist/vuetify.min.css'
 
+import './styles.css'
+
 import TimeSlider from "../src/components/TimeSlider.vue"
+import SvgTimeSlider from "../src/components/SvgTimeSlider.vue"
 
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
 
+const settings =
 storiesOf("TimeSlider", module).add("default", () => ({
   components: { TimeSlider },
   template: "<time-slider></time-slider>"
 }))
+
+storiesOf("SvgTimeSlider", module)
+  .add("default", () => ({
+    vuetify,
+    components: { SvgTimeSlider },
+    data () {
+      return {
+        startDate: moment('2010-01-01'),
+        endDate: moment('2019-01-01')
+      }
+    },
+    template: `
+<v-app>
+  <svg-time-slider
+    :start-date="startDate"
+    :end-date="endDate"
+  >
+  <div>test</div>
+  </svg-time-slider>
+</v-app>
+`
+  }))
