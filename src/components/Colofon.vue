6002694cb5f9ddb0c3f163338747f0bf8042c199
gena <gennadiy.donchyts@gmail.com>
2019-11-07 16:06:26 +0100

    enable multiple layer selection, add scrollbar, fix colofon text

--- src/components/Colofon.vue
+++ src/components/Colofon.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-    <h1 class="pa-4">
-      Colofon
-    </h1>
-    <v-card small flat>
-      <v-card-text class="py-0">
-        <p>
-          Voor algemene vragen over de vegetatiemonitor en de achtergronden bij
-          deze ontwikkeling:
-          <a href="mailto:?subject=Vegetatiemonitor">Rijkswaterstaat</a>
-          Voor technische vragen:
-          <a href="mailto:?subject=Vegetatiemonitor">Deltares</a>
-          Download <a href="static/docs/TRM.pdf" download>hier</a> de Technische
-          referentie handleiding. Klik
-          <a href="https://www.youtube.com/watch?v=mOjCRgauxyM&feature=youtu.be"
-            >hier</a
-          >
-          voor het instructiefilmpje.
-        </p>
-      </v-card-text>
-    </v-card>
+      <h1 class="pa-4">
+        Colofon
+      </h1>
+      <v-card small flat max-width="338">
+        <v-card-text class="py-0">
+            Voor algemene vragen over de vegetatiemonitor en de achtergronden bij
+            deze ontwikkeling:
+            <a href="mailto:?subject=Vegetatiemonitor">Rijkswaterstaat</a>
+            Voor technische vragen:
+            <a href="mailto:?subject=Vegetatiemonitor">Deltares</a>
+            Download <a href="static/docs/TRM.pdf" download>hier</a> de Technische
+            referentie handleiding. Klik
+            <a href="https://www.youtube.com/watch?v=mOjCRgauxyM&feature=youtu.be"
+              >hier</a
+            >
+            voor het instructiefilmpje.
+        </v-card-text>
+      </v-card>
   </div>
 </template>
 
